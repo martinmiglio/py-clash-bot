9e436d485d2ba8159bdd3f2f2d970d23a2dd23c0
Martin Miglio <marmig0404@gmail.com>
2024-06-24 00:08:10 -0400

    add timing

--- src/pyclashbot/detection/inference/run.py
+++ src/pyclashbot/detection/inference/run.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from time import perf_counter
 
 import cv2
 
@@ -10,33 +11,66 @@ from pyclashbot.memu.pmc import pmc
 
 logging.basicConfig(level=logging.INFO)
 
-current_path = os.path.dirname(os.path.realpath(__file__))
-model_path = os.path.join(current_path, "model.onnx")
-
-logging.info(f"Loading model from {model_path}")
-detector = UnitDetector(model_path)
-logging.info("Model loaded")
-
-
 logging.info("Starting VM")
 vm_index = 3
 pmc.start_vm(vm_index)
 logging.info("VM started")
 
-logging.info("Starting detection loop")
-while True:
-    try:
-        ss = screenshot(vm_index)
-        image = detector.preprocess(ss)
-        pred = detector.run(image)
-        image = draw_bboxes(ss, pred, pred_dims=(640, 640))
-        cv2.imshow("Predictions", image)
-        if cv2.waitKey(25) == 27:
-            raise KeyboardInterrupt
-    except Exception as e:
-        logging.error(type(e))
-        logging.error(e)
-    except KeyboardInterrupt:
-        break
+current_path = os.path.dirname(os.path.realpath(__file__))
+model_path = os.path.join(current_path, "model.onnx")
 
-cv2.destroyAllWindows()
+
+def detection(detector):
+    durations: dict[str, tuple[float, float]] = {}
+    start = perf_counter()
+
+    ss = screenshot(vm_index)
+    durations["screenshot"] = (start, perf_counter())
+
+    image = detector.preprocess(ss)
+    durations["preprocess"] = (durations["screenshot"][1], perf_counter())
+
+    pred = detector.run(image)
+    durations["inference"] = (durations["preprocess"][1], perf_counter())
+
+    image = draw_bboxes(ss, pred, pred_dims=(640, 640))
+    cv2.imshow("Predictions", image)
+    if cv2.waitKey(25) == 27:
+        raise KeyboardInterrupt
+    durations["draw"] = (durations["inference"][1], perf_counter())
+
+    durations["total"] = (durations["screenshot"][0], durations["draw"][1])
+    return {k: v[1] - v[0] for k, v in durations.items()}
+
+
+def detection_loop(use_gpu):
+    logging.info(f"Loading model from {model_path}")
+    detector = UnitDetector(model_path, use_gpu)
+    logging.info("Model loaded")
+
+    logging.info("Starting detection loop")
+
+    durations = []
+    while True:
+        try:
+            duration = detection(detector)
+            durations.append(duration)
+        except Exception as e:
+            logging.error(type(e))
+            logging.error(e)
+        except KeyboardInterrupt:
+            break
+
+    cv2.destroyAllWindows()
+    logging.info("Detection loop ended")
+    return durations
+
+
+if __name__ == "__main__":
+    durations = detection_loop(use_gpu=True)
+
+    import pandas as pd
+
+    durations = pd.DataFrame(durations)
+    durations = durations.iloc[1:]
+    logging.info("\n" + str(durations.describe()))
